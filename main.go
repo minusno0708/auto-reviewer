cf36f383a55038a4ea45ba70247cb278460d394f
minusno0708 <naoyao1014@gmail.com>
2024-07-12 22:26:49 +0900

    feat:プロンプトにdiffの内容を組み込む

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 var promptPath = "prompt.json"
+var diffPath = "diff.txt"
 
 func main() {
 	URL := "https://api.openai.com/v1/chat/completions"
@@ -19,11 +22,11 @@ func main() {
 	loadEnv()
 	apiKey := os.Getenv("API_KEY")
 
-	prompt := readFile(promptPath)
+	prompt := getPrompt()
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(prompt))
+	req, err := http.NewRequest("POST", URL, bytes.NewBuffer([]byte(prompt)))
 	if err != nil {
 		log.Fatal("Error reading request. ", err)
 	}
@@ -50,6 +53,19 @@ func loadEnv() {
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
+}
+
+func getPrompt() string {
+	rawDiff := string(readFile(diffPath))
+	basePrompt := string(readFile(promptPath))
+
+	// " や \n などの特殊文字をエスケープ
+	formatDiff := strconv.Quote(rawDiff)
+	formatDiff = formatDiff[1 : len(formatDiff)-1]
+
+	concatPrompt := strings.Replace(basePrompt, "[DIFF]", formatDiff, -1)
+
+	return concatPrompt
 }
 
 func readFile(filePath string) []byte {
